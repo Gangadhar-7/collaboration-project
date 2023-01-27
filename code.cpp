7799f2a86787a3521d8cf1d10a9858578d16c349
Gangadhar-7 <gangadharm877@gmail.com>
2023-01-27 14:29:33 +0530

    new project

---
+++ code.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Takeinput() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}  
+int main() {
+    Takeinput();
+    string s = "abc";
+    
+    cout<<s*3;
+    
+}
+
+
